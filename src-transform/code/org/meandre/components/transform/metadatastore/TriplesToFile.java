e004054c17fc4d757bb0dda40b8bb8d66fda7631
Lily Dong <lilydong@illinois.edu>
2008-07-07 15:32:29 +0000

    check in metadata

---
+++ src-transform/code/org/meandre/components/transform/metadatastore/TriplesToFile.java
@@ -0,0 +1,276 @@
+/**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright © 2008, NCSA.  All rights reserved.
+ * 
+ * Developed by:
+ * The Automated Learning Group
+ * University of Illinois at Urbana-Champaign
+ * http://www.seasr.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal with the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimers.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimers in
+ * the documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the names of The Automated Learning Group, University of
+ * Illinois at Urbana-Champaign, nor the names of its contributors may
+ * be used to endorse or promote products derived from this Software
+ * without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+ */
+
+package org.meandre.components.transform.metadatastore;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.httpclient.HttpHost;
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentContextProperties;
+import org.meandre.core.ComponentExecutionException;
+import org.meandre.core.ExecutableComponent;
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentProperty;
+import org.meandre.tools.webdav.WebdavClient;
+
+/**
+ * This component reads RDF triples from the input and writes them to a file.
+ * This has been created as a complement to the XmlToTriples component and uses
+ * that component's outputs to feed this component's inputs.
+ *
+ * @author Boris Capitanu
+ * @jira COMPONENTS-11 Create TriplesToFile component
+ */
+
+@Component(
+        creator = "Boris Capitanu",
+        description = "Saves RDF triples to file",
+        name = "TriplesToFile",
+        tags = "rdf triples",
+        firingPolicy = Component.FiringPolicy.any
+)
+public class TriplesToFile implements ExecutableComponent {
+    @ComponentInput(description = "Source base URL", name = "src_base_url")
+    final static String DATA_INPUT_SRC_BASE_URL = "src_base_url";
+
+    @ComponentInput(description = "Destination base URL", name = "dest_base_url")
+    final static String DATA_INPUT_DEST_BASE_URL = "dest_base_url";
+
+    @ComponentInput(description = "Document URL", name = "doc_url")
+    final static String DATA_INPUT_DOC_URL = "doc_url";
+
+    @ComponentInput(description = "Triples", name = "triples")
+    final static String DATA_INPUT_TRIPLES = "triples";
+
+    @ComponentProperty(description = "Processing method (header | body)",
+                       name = "processing_method", defaultValue = "body")
+    final static String DATA_PROPERTY_PROCESSING_METHOD = "processing_method";
+
+    private Logger _logger;
+
+    private File _tempFile;
+    private URL _urlBaseSrc;
+    private URL _urlBaseDest;
+    private URL _urlDoc;
+    private Queue<String> _queueTriples;
+    private boolean _bDone;
+    private boolean _bIsProcessingHeader;
+
+    /*
+     * (non-Javadoc)
+     * @see org.meandre.core.ExecutableComponent#initialize(org.meandre.core.ComponentContextProperties)
+     */
+    public void initialize(ComponentContextProperties contextProperties) {
+    	_logger = contextProperties.getLogger();
+
+        _urlBaseSrc = _urlBaseDest = _urlDoc = null;
+        _queueTriples = new LinkedList<String>();
+        _bDone = false;
+
+        String strProcessingMethod = contextProperties.getProperty(DATA_PROPERTY_PROCESSING_METHOD);
+        _bIsProcessingHeader = strProcessingMethod.toLowerCase().equals("header");
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.meandre.core.ExecutableComponent#execute(org.meandre.core.ComponentContext)
+     */
+    public void execute(ComponentContext context) throws ComponentExecutionException, ComponentContextException {
+        _logger.entering(this.getClass().getName(), "execute");
+
+        // Get the source base URL, if available
+        if (context.isInputAvailable(DATA_INPUT_SRC_BASE_URL) && _urlBaseSrc == null)
+            _urlBaseSrc = getURL((String) context.getDataComponentFromInput(DATA_INPUT_SRC_BASE_URL));
+
+        // Get the destination base URL, if available
+        if (context.isInputAvailable(DATA_INPUT_DEST_BASE_URL) && _urlBaseDest == null)
+            _urlBaseDest = getURL(context.getDataComponentFromInput(DATA_INPUT_DEST_BASE_URL) + "/");
+
+        // Get the input document URL, if available
+        if (context.isInputAvailable(DATA_INPUT_DOC_URL) && _urlDoc == null)
+            _urlDoc = getURL((String) context.getDataComponentFromInput(DATA_INPUT_DOC_URL));
+
+        // Queue any new triples that arrived on the input port
+        if (context.isInputAvailable(DATA_INPUT_TRIPLES))
+            if (!_queueTriples.offer((String) context.getDataComponentFromInput(DATA_INPUT_TRIPLES)))
+                throw new ComponentExecutionException("Cannot add triple to internal queue");
+
+        if (_urlDoc != null && _tempFile == null) {
+            // Create a temporary file to store the triples
+            String strTempFileName = _urlDoc.getPath().substring(_urlDoc.getPath().lastIndexOf("/") + 1);
+            try {
+                _tempFile = File.createTempFile(strTempFileName + "_", null);
+            }
+            catch (IOException e) {
+                String strError = "Cannot create temporary file";
+                _logger.log(Level.SEVERE, strError, e);
+                throw new ComponentExecutionException(strError);
+            }
+        }
+
+        if (_tempFile != null) {
+            while (!_queueTriples.isEmpty() && !_bDone) {
+                /*
+                 * If there are triples in the queue and we aren't yet done with the current document
+                 * then get the triples from the queue and append them to the temporary file
+                 */
+                String strTriple = _queueTriples.remove();
+                if (strTriple.trim().equals("EOF")) {
+                    _logger.info("Finished processing " + _urlDoc.toString());
+                    _bDone = true;
+                }
+                else {
+                    try {
+                        BufferedWriter writer = new BufferedWriter(new FileWriter(_tempFile, true));
+                        writer.write(strTriple);
+                        writer.close();
+                    }
+                    catch (IOException e) {
+                        String strError = "Cannot write to file";
+                        _logger.log(Level.SEVERE, strError, e);
+                        throw new ComponentExecutionException(strError);
+                    }
+                }
+            }
+        }
+
+        if (_urlBaseSrc != null && _urlBaseDest != null && _urlDoc != null && _bDone) {
+            /*
+             * If we know the base URL, the destination URL, and document URL, and we're done
+             * with the current file, then we can upload the teporary file to WebDAV
+             */
+
+            // Construct the output document name
+            String strDocSaveName = _urlDoc.getPath().substring(_urlDoc.getPath().lastIndexOf("/") + 1);
+            strDocSaveName += (_bIsProcessingHeader ? "_header" : "_body") + ".n3";
+
+            try {
+                // Construct the output URI for the document
+                URI uriDocRelPath = _urlBaseSrc.toURI().relativize(_urlDoc.toURI()).resolve(strDocSaveName);
+                URI uriOut = _urlBaseDest.toURI().resolve(uriDocRelPath);
+                _logger.info("Destination store is '" + uriOut.toString() + "'");
+
+                // Write the file to webdav now
+                String sDirectory = uriOut.toString();
+                sDirectory = sDirectory.substring(0, sDirectory.lastIndexOf("/"));
+
+                WebdavClient webdav = new WebdavClient(new HttpHost(uriOut.getHost(), uriOut.getPort()));
+                try {
+                    // Create the necessary webdav folders
+                    if (webdav.mkdirs(sDirectory)) {
+                        // ... and upload the file
+                        webdav.put(uriOut.toString(), _tempFile, "text/plain");
+                    }
+                    else
+                        _logger.severe("Could not create all subpaths for " + sDirectory);
+
+                    _logger.info("Finished uploading " + uriOut.toString());
+                }
+                catch (IOException e) {
+                    String strError = "Error uploading '" + uriOut.toString() + "' to WebDAV: ";
+                    _logger.log(Level.SEVERE, strError, e);
+                    throw new ComponentExecutionException(strError);
+                }
+                finally {
+                    _tempFile.delete();
+                }
+
+                // Reset state for next document
+                _bDone = false;
+                _urlDoc = null;
+                _tempFile = null;
+
+            }
+            catch (URISyntaxException e) {
+            	_logger.log(Level.SEVERE, "Execution error: ", e);
+                throw new ComponentExecutionException(e.getMessage());
+            }
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.meandre.core.ExecutableComponent#dispose(org.meandre.core.ComponentContextProperties)
+     */
+    public void dispose(ComponentContextProperties ccp) {
+    }
+
+    /**
+     * Converts a file path or file url to a URL
+     *
+     * @param location the file path or URL specifying the location
+     * @return the URL describing that location
+     * @throws ComponentContextException Thrown if the location cannot be converted to a URL
+     */
+    private URL getURL(String location) throws ComponentContextException {
+        URL url;
+        try {
+            url = new URL(location);
+        }
+        catch (MalformedURLException e) {
+            File file = new File(location);
+            try {
+                URI uri = file.toURI();
+                url = uri.toURL();
+            }
+            catch (MalformedURLException e1) {
+                String strError = "Cannot convert '" + location + "' to a URL: ";
+                _logger.log(Level.SEVERE, strError, e1);
+                throw new ComponentContextException(strError);
+            }
+        }
+
+        return url;
+    }
+}
---
+++ src-transform/code/org/meandre/components/transform/metadatastore/XmlToTriples.java
@@ -0,0 +1,271 @@
+/**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright © 2008, NCSA.  All rights reserved.
+ * 
+ * Developed by:
+ * The Automated Learning Group
+ * University of Illinois at Urbana-Champaign
+ * http://www.seasr.org
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal with the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ * 
+ * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimers.
+ * 
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimers in
+ * the documentation and/or other materials provided with the distribution.
+ * 
+ * Neither the names of The Automated Learning Group, University of
+ * Illinois at Urbana-Champaign, nor the names of its contributors may
+ * be used to endorse or promote products derived from this Software
+ * without specific prior written permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+ */
+
+package org.meandre.components.transform.metadatastore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentContextProperties;
+import org.meandre.core.ComponentExecutionException;
+import org.meandre.core.ExecutableComponent;
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentOutput;
+import org.meandre.annotations.ComponentProperty;
+import org.meandre.components.transform.metadatastore.support.IRdfStatementProcessor;
+import org.meandre.components.transform.metadatastore.support.XmlException;
+import org.meandre.components.transform.metadatastore.support.XmlToRdfConverter;
+
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Statement;
+
+/**
+ * This component takes as input a file path or URL specifying a TEI-encoded XML data source,
+ * parses the header (identified by 'teiHeader') and body (identified by 'text') and
+ * converts the XML data to RDF triples, pushing them to their respective outputs as they are produced.
+ *
+ * @author Boris Capitanu
+ * @jira COMPONENTS-10 Create XmlToTriples component
+ */
+@Component(
+        creator = "Boris Capitanu",
+        description = "Converts TEI-encoded XML data to RDF triples. " +
+        		"The TEI-XML format contains two sections ('teiHeader' and 'text') which " +
+        		"this component processes separately to generate triples for each section.<br/>" +
+        		"<u>Note:</u> The end-of-section processing is indicated by pushing out the string: 'EOF'",
+        name = "XmlToTriples",
+        tags = "rdf, xml, triples, tei, converter"
+)
+public class XmlToTriples implements ExecutableComponent, IRdfStatementProcessor {
+    @ComponentInput(description = "Location of XML data", name = "url")
+    final static String DATA_INPUT_URL = "url";
+
+    @ComponentOutput(description = "The triples corresponding to the 'teiHeader' section", 
+    		name = "header_triples")
+    final static String DATA_OUTPUT_HEADER_TRIPLES = "header_triples";
+
+    @ComponentOutput(description = "The triples corresponding to the 'text' section", name = "body_triples")
+    final static String DATA_OUTPUT_BODY_TRIPLES = "body_triples";
+
+    @ComponentOutput(description = "The URL for the document currently being processed", name = "doc_url")
+    final static String DATA_OUTPUT_DOC_URL = "doc_url";
+
+    @ComponentProperty(description = "Normalize whitespaces? (true | false)",
+                       name = "normalizeWhitespaces", defaultValue = "true")
+    final static String DATA_PROPERTY_NORMALIZEWHITESPACES = "normalizeWhitespaces";
+
+    @ComponentProperty(description = "Create attribute sequences? (true | false)",
+                       name = "createAttrSeq", defaultValue = "true")
+    final static String DATA_PROPERTY_CREATEATTRSEQ = "createAttrSeq";
+
+    @ComponentProperty(description = "Stop flow on error? (true | false)",
+                       name = "stopOnError", defaultValue = "true")
+    final static String DATA_PROPERTY_STOPONERROR = "stopOnError";
+
+    private Logger _logger;
+    private ComponentContext _context;
+    private URL _url;
+
+    /*
+     * (non-Javadoc)
+     * @see org.meandre.core.ExecutableComponent#initialize(org.meandre.core.ComponentContextProperties)
+     */
+    public void initialize(ComponentContextProperties context) {
+        _logger = context.getLogger();
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.meandre.core.ExecutableComponent#execute(org.meandre.core.ComponentContext)
+     */
+    public void execute(ComponentContext context) throws ComponentExecutionException, ComponentContextException {
+        _logger.entering(this.getClass().getName(), "execute");
+
+        _context = context;
+
+        // Create a URL for the source XML data
+        _url = getURL((String) context.getDataComponentFromInput(DATA_INPUT_URL));
+        if (_url == null)
+            throw new ComponentContextException("Invalid input URL specified");
+
+        try {
+            _context.pushDataComponentToOutput(DATA_OUTPUT_DOC_URL, _url.toString());
+        }
+        catch (Exception e) {
+            _logger.log(Level.SEVERE, "Execution error: ", e);
+        }
+
+        // Get the component configuration values from the supplied properties
+        boolean bNormalizeWhitespaces = Boolean.parseBoolean(context.getProperty(DATA_PROPERTY_NORMALIZEWHITESPACES));
+        boolean bCreateAttrSeq = Boolean.parseBoolean(context.getProperty(DATA_PROPERTY_CREATEATTRSEQ));
+        boolean bStopOnError = Boolean.parseBoolean(context.getProperty(DATA_PROPERTY_STOPONERROR));
+
+        // Perform the actual conversion
+        try {
+            new XmlToRdfConverter(_url, bCreateAttrSeq, bNormalizeWhitespaces, this).start();
+        }
+        catch (IOException e) {
+            String strError = "Processing error - '" + _url.toString() + "': ";
+            _logger.log(Level.SEVERE, strError, e);
+            if (bStopOnError) throw new ComponentExecutionException(strError);
+        }
+        catch (XmlException e) {
+            String strError = "Processing error - '" + _url.toString() + "': ";
+            _logger.log(Level.SEVERE, strError, e);
+            if (bStopOnError) throw new ComponentExecutionException(strError);
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.meandre.core.ExecutableComponent#dispose(org.meandre.core.ComponentContextProperties)
+     */
+    public void dispose(ComponentContextProperties context) {
+    }
+
+    /**
+     * Converts a file path or file _url to a URL
+     *
+     * @param location the file path or URL specifying the location
+     * @return the URL describing that location
+     */
+    private URL getURL(String location) {
+        URL url = null;
+
+        try {
+            url = new URL(location);
+        }
+        catch (MalformedURLException e) {
+            File file = new File(location);
+            try {
+                URI uri = file.toURI();
+                url = uri.toURL();
+            }
+            catch (MalformedURLException e1) {
+                _logger.log(Level.SEVERE, "Cannot convert '" + location + "' to a URL: ", e1);
+            }
+        }
+
+        return url;
+    }
+
+    /**
+     * Called before the processing of the 'teiHeader' section
+     */
+    public void startHeaderProcessing() {
+    	_logger.fine(_url.toString());
+    }
+
+    /**
+     * Called after the processing of the 'teiHeader' section is complete
+     */
+    public void endHeaderProcessing() {
+        try {
+        	// Push out the custom EOF marker to indicate the completion of processing
+            _context.pushDataComponentToOutput(DATA_OUTPUT_HEADER_TRIPLES, "EOF");
+            _logger.fine(_url.toString());
+        }
+        catch (ComponentContextException e) {
+            _logger.log(Level.SEVERE, "Processing error: ", e);
+        }
+    }
+
+    /**
+     * Called whenever a header triple has been generated
+     * 
+     * @param stmt the newly-generated header triple
+     */
+    public void processHeaderStatement(Statement stmt) {
+        try {
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            ModelFactory.createDefaultModel().add(stmt).write(outStream, "N-TRIPLE", null);
+            _context.pushDataComponentToOutput(DATA_OUTPUT_HEADER_TRIPLES, outStream.toString());
+        }
+        catch (ComponentContextException e) {
+            _logger.log(Level.SEVERE, "Processing error: ", e);
+        }
+    }
+
+    /**
+     * Called before the body ('text') section is processed
+     */
+    public void startBodyProcessing() {
+    	_logger.fine(_url.toString());
+    }
+
+    /**
+     * Called after the body ('text') section has been processed
+     */
+    public void endBodyProcessing() {
+        try {
+        	// Push out the custom EOF marker to indicate the completion of processing
+            _context.pushDataComponentToOutput(DATA_OUTPUT_BODY_TRIPLES, "EOF");
+            _logger.fine(_url.toString());
+        }
+        catch (ComponentContextException e) {
+            _logger.log(Level.SEVERE, "Processing error: ", e);
+        }
+    }
+
+    /**
+     * Called whenever a body triple has been generated
+     * 
+     * @param stmt the newly-generated body triple
+     */
+    public void processBodyStatement(Statement stmt) {
+        try {
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            ModelFactory.createDefaultModel().add(stmt).write(outStream, "N-TRIPLE", null);
+            _context.pushDataComponentToOutput(DATA_OUTPUT_BODY_TRIPLES, outStream.toString());
+        }
+        catch (ComponentContextException e) {
+            _logger.log(Level.SEVERE, "Processing error: ", e);
+        }
+    }
+}
