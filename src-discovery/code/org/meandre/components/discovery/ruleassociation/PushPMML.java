1d3727c9b22ffcbe529d4ff103a70ad30082cdd2
Lily Dong <lilydong@illinois.edu>
2009-04-12 20:30:40 +0000

    new component for saving PMML

---
+++ src-discovery/code/org/meandre/components/discovery/ruleassociation/PushPMML.java
@@ -0,0 +1,93 @@
+/**
+ * University of Illinois/NCSA
+ * Open Source License
+ *
+ * Copyright (c) 2008, Board of Trustees-University of Illinois.
+ * All rights reserved.
+ *
+ * Developed by:
+ *
+ * Automated Learning Group
+ * National Center for Supercomputing Applications
+ * http://www.seasr.org
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal with the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimers.
+ *
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimers in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ *  * Neither the names of Automated Learning Group, The National Center for
+ *    Supercomputing Applications, or University of Illinois, nor the names of
+ *    its contributors may be used to endorse or promote products derived from
+ *    this Software without specific prior written permission.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * WITH THE SOFTWARE.
+ */
+
+package org.meandre.components.discovery.ruleassociation;
+
+import java.io.FileWriter;
+
+import org.dom4j.Document;
+import org.dom4j.io.XMLWriter;
+import org.dom4j.io.OutputFormat;
+
+import org.meandre.annotations.Component;
+import org.meandre.annotations.ComponentInput;
+import org.meandre.annotations.ComponentProperty;
+import org.meandre.core.ComponentContext;
+import org.meandre.core.ComponentContextException;
+import org.meandre.core.ComponentContextProperties;
+import org.meandre.core.ComponentExecutionException;
+import org.meandre.core.ExecutableComponent;
+
+@Component(creator="Lily Dong",
+           description="Saves the data complying with PMML(Predictive Model Markup Language) format as local file.",
+           name="Push PMML",
+           tags="frequent pattern mining, rule association, PMML")
+
+public class PushPMML  implements ExecutableComponent{
+	@ComponentProperty(description = "Local file name.",
+             		   name = "fileName",
+             		   defaultValue = " ")
+    final static String DATA_PROPERTY = "fileName";
+
+	@ComponentInput(description="Read data being of PMML format." +
+	    		"<br>TYPE: org.dom4j.Document",
+	                name= "document")
+	final static String DATA_INPUT = "document";
+
+	public void initialize(ComponentContextProperties ccp) {}
+	public void dispose(ComponentContextProperties ccp) {}
+
+	 public void execute(ComponentContext cc)
+     throws ComponentExecutionException, ComponentContextException {
+		 Document document = (Document)cc.getDataComponentFromInput(DATA_INPUT);
+		 try {
+			 XMLWriter writer = new XMLWriter(
+					 new FileWriter(cc.getProperty(DATA_PROPERTY)),
+	                 OutputFormat.createPrettyPrint());
+	         writer.write(document);
+	         writer.flush();
+	         writer.close();
+	     } catch (Exception e) {
+	    	 throw new ComponentExecutionException(e);
+	     }
+	 }
+}
