9005809c184fc97118a9e763f5b2932b8c293ac2
Lily Dong <lilydong@illinois.edu>
2009-05-06 15:48:21 +0000

    fix a bug causing class missing error  in browser.

--- src-viz/code/org/meandre/applet/prediction/decisiontree/widgets/TreeScrollPane.java
+++ src-viz/code/org/meandre/applet/prediction/decisiontree/widgets/TreeScrollPane.java
@@ -1,36 +1,36 @@
 /**
  * University of Illinois/NCSA
  * Open Source License
- * 
- * Copyright (c) 2008, Board of Trustees-University of Illinois.  
+ *
+ * Copyright (c) 2008, Board of Trustees-University of Illinois.
  * All rights reserved.
- * 
- * Developed by: 
- * 
+ *
+ * Developed by:
+ *
  * Automated Learning Group
  * National Center for Supercomputing Applications
  * http://www.seasr.org
- * 
- *  
+ *
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal with the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
  * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions: 
- * 
+ * furnished to do so, subject to the following conditions:
+ *
  *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimers. 
- * 
+ *    this list of conditions and the following disclaimers.
+ *
  *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimers in the 
- *    documentation and/or other materials provided with the distribution. 
- * 
+ *    this list of conditions and the following disclaimers in the
+ *    documentation and/or other materials provided with the distribution.
+ *
  *  * Neither the names of Automated Learning Group, The National Center for
  *    Supercomputing Applications, or University of Illinois, nor the names of
  *    its contributors may be used to endorse or promote products derived from
- *    this Software without specific prior written permission. 
- * 
+ *    this Software without specific prior written permission.
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
@@ -38,12 +38,18 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * WITH THE SOFTWARE.
- */ 
+ */
 
 package org.meandre.applet.prediction.decisiontree.widgets;
 
 import org.meandre.components.prediction.decisiontree.support.ViewableDTModel;
 import org.meandre.components.prediction.decisiontree.support.ViewableDTNode;
+
+//-------
+import org.meandre.components.prediction.decisiontree.c45.support.NumericDecisionTreeNode;
+import org.meandre.components.prediction.decisiontree.c45.support.CategoricalDecisionTreeNode;
+import org.meandre.components.prediction.decisiontree.c45.support.DecisionTreeNode;
+//-------
 
 import javax.swing.*;
 
@@ -289,6 +295,16 @@ public final class TreeScrollPane extends JScrollPane {
             brushpanel = panel;
 
             root = model.getViewableRoot();
+
+            //-------
+            if(root instanceof NumericDecisionTreeNode)
+            	System.out.println("-- 1 --");
+            else if(root instanceof CategoricalDecisionTreeNode)
+            	System.out.println("-- 2 --");
+            else if(root instanceof DecisionTreeNode)
+            	System.out.println("-- 3 --");
+            //-------
+
             viewportroot = new Viewport(model, root);
 
             findMaximumDepth(root);
